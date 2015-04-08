fc2dfa2cb22ea300147ada3b5fcf4d199350d666
Cap Van Vu <capvanvu@gmail.com>
2015-04-08 17:35:23 +0700

    ll

--- Crystal.cs
+++ Crystal.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class BasicBlockExplore : MonoBehaviour {
 
 	public Sprite[] listSprites;
+	private int[] frames = [0,0,0,0,1,1,1,2,2,2];
 	float startTime = Time.time;
 	int frame = 0;
 	// Use this for initialization
 	void Start () {
+		//Random frame
+		//Ramdom speed
+		//
 		this.GetComponent<SpriteRenderer>().sprite = listSprites[0];
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Time.time - startTime > 0.1f) 
+		if (Time.time - startTime > 0.1f)
 		{
 			startTime = Time.time;
 			frame ++;
@@ -26,5 +30,10 @@ public class BasicBlockExplore : MonoBehaviour {
 				Destroy (this.gameObject);
 			}
 		}
+	}
+
+	public void setTarget(int x, int y)
+	{
+
 	}
 }
